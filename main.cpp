d4ec21545c2ef1cdabd225750288e75bd0ac47d1
mipxe <mariamozegova8726@gmail.com>
2024-10-17 00:29:48 +0300

    error handing and add input/output

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@ int main()
 {
   size_t M = 0, N = 0;
   std::cin >> M >> N;
-  int** t  = creatematrix(M,N);
+  int** t  = nullptr;
+  try {
+    t = creatematrix(M,N);
+  } catch (const std::bad_alloc & e) {
+    std::cerr << "Out of memory\n";
+    return 1;
+  }
+  input(t, M, N);
+  output(t, M, N);
   destroy(t, M, N);
 }

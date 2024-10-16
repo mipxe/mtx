050af00ecebb795616e843e3d1fabaebe1cc3a62
mipxe <mariamozegova8726@gmail.com>
2024-10-17 00:23:13 +0300

    add functions input and output

--- matrix.hpp
+++ matrix.hpp
@@ -2,4 +2,6 @@
 #define MATRIX_HPP
 void destroy(int** t, size_t m);
 int** creatematrix(size_t m, size_t n);
+void input(int** t, size_t m, size_t n);
+void output(const int * const * t, size_t m, size_t n);
 #endif

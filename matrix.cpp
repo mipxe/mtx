afb3f466fdee1cb2a8052cffb4f59bbac820f9c3
mipxe <mariamozegova8726@gmail.com>
2024-10-16 19:05:02 +0300

    creatematrix

--- matrix.cpp
+++ matrix.cpp
@@ -6,6 +6,7 @@ void input(int** t, size_t m, size_t n) {
     }
   }
 }
+
 void output(const int * const * t, size_t m, size_t n) {
   for (size_t i = 0; i < m, ++i) {
     std::cout << t[i][0];
@@ -15,9 +16,24 @@ void output(const int * const * t, size_t m, size_t n) {
     std::cout << "\n";
   }
 }
+
 void destroy(int** t, size_t m) {
   for (size_t i = 0; i < m; ++i) {
     delete[] t[i];
   }
   delete[]t;
+}
+
+int ** creatematrix(size_t m, size_t n) {
+  int ** t = new int*[m];
+  size_t created = 0;
+  try {
+    for (;created < m; ++created) {
+      t[created] = new int[n];
+    }
+  } catch (const std::bad_alloc & e) {
+    destroy(t, created);
+    throw;
+  }
+  return t;
 }

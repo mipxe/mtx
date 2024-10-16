a42a59f42f6ddd0b37630878329e92065a068cf5
mipxe <mariamozegova8726@gmail.com>
2024-10-17 00:14:42 +0300

    fixed fuctions

--- matrix.cpp
+++ matrix.cpp
@@ -8,10 +8,10 @@ void input(int** t, size_t m, size_t n) {
 }
 
 void output(const int * const * t, size_t m, size_t n) {
-  for (size_t i = 0; i < m, ++i) {
+  for (size_t i = 0; i < m; ++i) {
     std::cout << t[i][0];
-    for (size_t j = 0; j < n; ++j) {
-      std::cout << t[i][j];
+    for (size_t j = 1; j < n; ++j) {
+      std::cout << " " << t[i][j];
     }
     std::cout << "\n";
   }

b77cd2a03af6bdac82137ced94db277628174e07
mipxe <mariamozegova8726@gmail.com>
2024-11-05 11:50:10 +0300

    create class matrix

--- matrix.hpp
+++ matrix.hpp
@@ -1,8 +1,27 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
-#include <cstddef>
-void destroy(int** t, size_t m);
-int** creatematrix(size_t m, size_t n);
-void input(int** t, size_t m, size_t n);
-void output(const int * const * t, size_t m, size_t n);
+
+#include <iostream>
+
+class Matrix
+{
+public:
+  Matrix(size_t m, size_t n);
+  Matrix(const Matrix & other);
+  ~Matrix();
+  size_t GetRow();
+  size_t GetColumn();
+  void input(std::istream & in);
+  void output(std::ostream & out);
+  void changeSize(size_t m, size_t n);
+private:
+  size_t m;
+  size_t n;
+  int ** t;
+  void destroy(int** t, size_t m);
+  int** creatematrix(size_t m, size_t n);
+  void input(std::istream & in, int** t, size_t m, size_t n);
+  void output(std::ostream & out, const int * const * t, size_t m, size_t n);
+};
+
 #endif
